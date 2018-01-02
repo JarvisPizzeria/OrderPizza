df39e0874cc7ac1186dae991ae908669fc45a373
Richard Olrichs <richard@olrichs.nl>
2018-01-02 22:02:43 +0100

    Init of a JetApp we might want to build and/or use in the future.

---
+++ JetApp/JarvisPizzeria/src/js/appController.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+/*
+ * Your application specific code will go here
+ */
+define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarraytabledatasource'],
+  function(oj, ko) {
+     function ControllerViewModel() {
+       var self = this;
+
+      // Media queries for repsonsive layouts
+      var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+      self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+
+       // Router setup
+       self.router = oj.Router.rootInstance;
+       self.router.configure({
+         'home': {label: 'Home', isDefault: true},
+         'team': {label: 'Team'},
+         'github': {label: 'Github'},
+         'blog': {label: 'Blog'}
+       });
+      oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
+
+      // Navigation setup
+      var navData = [
+      {name: 'Home', id: 'home',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
+      {name: 'Team', id: 'team',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
+      {name: 'Github', id: 'github',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'},
+      {name: 'Blog', id: 'blog',
+       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
+      ];
+      self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
+
+      // Header
+      // Application Name used in Branding Area
+      self.appName = ko.observable("Jarvis Pizzeria");
+      // User Info used in Global Navigation area
+      self.userLogin = ko.observable("richard@jarvispizzeria.nl");
+
+      // Footer
+      function footerLink(name, id, linkTarget) {
+        this.name = name;
+        this.linkId = id;
+        this.linkTarget = linkTarget;
+      }
+      self.footerLinks = ko.observableArray([        
+        //new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+        //new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+      ]);
+     }
+
+     return new ControllerViewModel();
+  }
+);
